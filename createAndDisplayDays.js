693c6da095d0da7c6b664769e1ca0e3fe381dbcd
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-02-08 15:10:35 +0000

    Updated createAndDisplayDays

--- createAndDisplayDays.js
+++ createAndDisplayDays.js
@@ -1,11 +1,11 @@
-import { createCeil } from "./createCeil.js";
+import { createHeaderCeil } from "./createHeaderCeil.js";
 import { weekdays } from "./data.js";
 const calendarEl = document.querySelector("#calendar-container");
 console.log(calendarEl);
 function createAndDisplayDays() {
   const thHeader = document.createElement("thead");
   const thR = document.createElement("tr");
-  const daysList = weekdays.map(createCeil);
+  const daysList = weekdays.map(createHeaderCeil);
   console.log(daysList);
   thR.append(...daysList);
   thHeader.append(thR);
