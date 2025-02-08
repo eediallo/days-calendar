d328a80ddcfced7e9d28dc4efc695ed64a90da1f
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-02-08 16:01:44 +0000

    Created populateDaysOfMonth function

--- data.js
+++ data.js
@@ -1,1 +1,13 @@
- export const weekdays=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+ export const weekdays=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+
+ function populateDaysOfMonth()
+ {
+    const daysOfMonth=[];
+    for(let i=1;i<=31;i++)
+        {
+            daysOfMonth.push(i);
+        }
+        return daysOfMonth;
+ }
+ 
+export {populateDaysOfMonth};
