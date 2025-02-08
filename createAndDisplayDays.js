2cb67850d024aa91a50f0146d67e1fdc1cf2d0bb
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-08 14:29:56 +0000

    update calendar element selector to use #calendar-container

--- createAndDisplayDays.js
+++ createAndDisplayDays.js
@@ -1,5 +1,5 @@
 import { createCeil } from "./createCeil.js";
-const calendarEl = document.querySelector("#calendar");
+const calendarEl = document.querySelector("#calendar-container");
 function createAndDisplayDays(days) {
   const daysList = days.map(createCeil);
   calendarEl.append(...daysList);
