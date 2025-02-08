98f526b4406e1df4d8075ae100f19bd3b05e827f
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-08 15:04:03 +0000

    Add table header for calendar days display

--- createAndDisplayDays.js
+++ createAndDisplayDays.js
@@ -1,11 +1,15 @@
 import { createCeil } from "./createCeil.js";
-import {weekdays} from "./data.js";
+import { weekdays } from "./data.js";
 const calendarEl = document.querySelector("#calendar-container");
 console.log(calendarEl);
 function createAndDisplayDays() {
+  const thHeader = document.createElement("thead");
+  const thR = document.createElement("tr");
   const daysList = weekdays.map(createCeil);
   console.log(daysList);
-  calendarEl.append(...daysList);
+  thR.append(...daysList);
+  thHeader.append(thR);
+  calendarEl.append(thHeader);
 }
 
 export { createAndDisplayDays };
