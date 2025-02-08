acdce4b2960d19fd94f1ce26ac8f275e2ce007d5
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-02-08 16:02:55 +0000

    Invoked createAndDisplayWeekDays function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 import { createAndDisplayDays } from "./createAndDisplayDays.js";
-
+import { createAndDisplayWeekDays } from "./createAndDisplayWeekDays.js";
 function main() {
   createAndDisplayDays();
+  createAndDisplayWeekDays();
 }
 
 window.onload = main;
