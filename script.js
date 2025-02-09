a76d60b2f9183fe52b06f0ed471f39d976cc45ef
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 21:28:22 +0000

    Replace renderMonthList with renderDropDowns in main function to initialize dropdowns

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 import { calendarConfig } from "./calendar/calendarConfig.js";
 import { displayCalendar } from "./calendar/displayCalendar.js";
 import { eventHandlers } from "./events/events.js";
-import { renderMonthList } from "./jump/renderMonthList.js";
+import { renderDropDowns } from "./jump/renderDropDown.js";
 
 function main() {
   displayCalendar(calendarConfig.currentDate);
   eventHandlers();
-  renderMonthList();
+  renderDropDowns();
 }
 window.onload = main;
