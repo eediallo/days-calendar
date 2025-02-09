138d9f119c37a26071063beb5bcaaf05c9af0082
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 20:39:45 +0000

    Update main function to pass current date to displayCalendar and initialize event handlers

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+import { calendarConfig } from "./calendar/calendarConfig.js";
 import { displayCalendar } from "./calendar/displayCalendar.js";
+import { eventHandlers } from "./events/events.js";
 
 function main() {
-  displayCalendar();
+  displayCalendar(calendarConfig.currentDate);
+  eventHandlers();
 }
 window.onload = main;
