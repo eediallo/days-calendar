b59c8545373bb800fc130f8070ac4cf3cce908ce
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 21:28:31 +0000

    Rename renderMonthList to renderMonthDropDown for clarity and consistency

--- jump/renderMonthList.js
+++ jump/renderMonthList.js
@@ -2,7 +2,7 @@ import { monthNames } from "../data/data.js";
 import { monthSelect } from "../queries.js";
 import { createOption } from "./createOption.js";
 
-export function renderMonthList() {
+export function renderMonthDropDown() {
   const optionList = monthNames.map((month) => createOption(month));
   monthSelect.append(...optionList);
 }
