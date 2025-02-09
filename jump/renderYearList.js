a83bcd97cc0fcbf3618757d630fd455557ccc130
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 21:31:51 +0000

    Refactor renderYearDropDown to use getYear method for current year calculation

--- jump/renderYearList.js
+++ jump/renderYearList.js
@@ -3,7 +3,7 @@ import { yearSelect } from "../queries.js";
 import { createOption } from "./createOption.js";
 
 export function renderYearDropDown() {
-  const currentYear = calendarConfig.currentDate.getFullYear();
+  const currentYear = calendarConfig.getYear(calendarConfig.currentDate);
   for (let year = currentYear - 10; year <= currentYear + 10; year++) {
     const option = createOption(year);
     yearSelect.append(option);
