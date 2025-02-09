66db89d1f512886416133cf243e62652eaaba732
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 21:39:36 +0000

    Set current month as selected in month dropdown

--- jump/renderMonthList.js
+++ jump/renderMonthList.js
@@ -1,8 +1,16 @@
+import { calendarConfig } from "../calendar/calendarConfig.js";
 import { monthNames } from "../data/data.js";
 import { monthSelect } from "../queries.js";
 import { createOption } from "./createOption.js";
 
 export function renderMonthDropDown() {
-  const optionList = monthNames.map((month) => createOption(month));
+  const currentMonth = calendarConfig.getMonth(calendarConfig.currentDate);
+  const optionList = monthNames.map((month, index) => {
+    const option = createOption(month);
+    if (index === currentMonth) {
+      option.selected = true;
+    }
+    return option;
+  });
   monthSelect.append(...optionList);
 }
