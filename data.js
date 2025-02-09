f161de1f46385cf31564855044e173c5d9bda544
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 12:43:44 +0000

    Add monthNames array and improve formatting of populateDaysOfMonth function

--- data.js
+++ data.js
@@ -1,13 +1,26 @@
- export const weekdays=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+export const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
- function populateDaysOfMonth()
- {
-    const daysOfMonth=[];
-    for(let i=1;i<=31;i++)
-        {
-            daysOfMonth.push(i);
-        }
-        return daysOfMonth;
- }
- 
-export {populateDaysOfMonth};
+function populateDaysOfMonth() {
+  const daysOfMonth = [];
+  for (let i = 1; i <= 31; i++) {
+    daysOfMonth.push(i);
+  }
+  return daysOfMonth;
+}
+
+export const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export { populateDaysOfMonth };
