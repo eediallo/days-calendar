a52901b4b6833f0c5cca8e5f8a1e7e66278315b4
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 12:44:04 +0000

    Create createMonthYearEl function to display the current month and year

---
+++ createMonthYearEl.js
@@ -0,0 +1,9 @@
+import { monthYearEl } from "./queries.js";
+import { monthNames } from "./data.js";
+
+const month = (date) => date.getMonth();
+const year = (date) => date.getFullYear();
+
+export function createMonthYearEl(date) {
+  monthYearEl.textContent = `${monthNames[month(date)]} ${year(date)}`;
+}
