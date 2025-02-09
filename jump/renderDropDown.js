3bfda152beece93167198035e1c4669f513b7ae4
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 21:28:12 +0000

    Add renderDropDowns function to initialize month and year dropdowns

---
+++ jump/renderDropDown.js
@@ -0,0 +1,7 @@
+import { renderMonthDropDown } from "./renderMonthList.js";
+import { renderYearDropDown } from "./renderYearList.js";
+
+export function renderDropDowns() {
+  renderYearDropDown();
+  renderMonthDropDown();
+}
