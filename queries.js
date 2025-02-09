b09652c9ff4d60eb149615e28771a7328db35c80
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 21:11:25 +0000

    Add month and year select elements to queries.js

--- queries.js
+++ queries.js
@@ -2,5 +2,14 @@ const monthYearEl = document.querySelector("#month-year");
 const calendarContainerEl = document.querySelector("#calendar-grid");
 const prevMonthEl = document.querySelector("#prev-month");
 const nexMonthEl = document.querySelector("#next-month");
+const monthSelect = document.querySelector("#month-select");
+const yearSelect = document.querySelector("#year-select");
 
-export { monthYearEl, calendarContainerEl, prevMonthEl, nexMonthEl };
+export {
+  monthYearEl,
+  calendarContainerEl,
+  prevMonthEl,
+  nexMonthEl,
+  monthSelect,
+  yearSelect,
+};
