fd53b786ab21e0a72831e3391073a68faf9aa7d4
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 20:39:53 +0000

    Refactor createMonthYearEl function to accept date parameter for dynamic month and year rendering

--- calendar/createMonthYearEl.js
+++ calendar/createMonthYearEl.js
@@ -2,8 +2,8 @@ import { monthYearEl } from "../queries.js";
 import { monthNames } from "../data/data.js";
 import { calendarConfig } from "./calendarConfig.js";
 
-export function createMonthYearEl() {
-  monthYearEl.textContent = `${monthNames[calendarConfig.month]} ${
-    calendarConfig.year
-  }`;
+export function createMonthYearEl(date) {
+  const month = calendarConfig.getMonth(date);
+  const year = calendarConfig.getYear(date);
+  monthYearEl.textContent = `${monthNames[month]} ${year}`;
 }
