a39aa220172d05787468afcc1e2e9eaef6f2607b
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 12:49:04 +0000

    Fix import path for populateDaysOfMonth in createAndDisplayWeekDays.js

--- createAndDisplayWeekDays.js
+++ createAndDisplayWeekDays.js
@@ -1,20 +1,19 @@
 import { createDayCeil } from "./createDayCeil.js";
-import { populateDaysOfMonth } from "./data.js";
+import { populateDaysOfMonth } from "./data/data.js";
 
+export function createAndDisplayWeekDays() {
+  const calendarEl = document.querySelector("#calendar-container");
+  const tableBody = document.createElement("tbody");
+  const daysOfMonth = populateDaysOfMonth().map(createDayCeil);
 
-export function createAndDisplayWeekDays(){
-    const calendarEl = document.querySelector("#calendar-container");
-    const tableBody = document.createElement("tbody");
-    const daysOfMonth = populateDaysOfMonth().map(createDayCeil);
-    
-    let tableRow;
-    daysOfMonth.forEach((day, index) => {
-        if (index % 7 === 0) {
-            tableRow = document.createElement("tr");
-            tableBody.append(tableRow);
-        }
-        tableRow.append(day);
-    });
+  let tableRow;
+  daysOfMonth.forEach((day, index) => {
+    if (index % 7 === 0) {
+      tableRow = document.createElement("tr");
+      tableBody.append(tableRow);
+    }
+    tableRow.append(day);
+  });
 
-    calendarEl.append(tableBody);
+  calendarEl.append(tableBody);
 }
