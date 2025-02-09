abc1ae21eb06fc4895f8fa08e9a448d359dd161c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 12:48:41 +0000

    Fix import path for createMonthYearEl in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { createAndDisplayDays } from "./createAndDisplayDays.js";
 import { createAndDisplayWeekDays } from "./createAndDisplayWeekDays.js";
-import { createMonthYearEl } from "./createMonthYearEl.js";
+import { createMonthYearEl } from "./calendar/createMonthYearEl.js";
 function main() {
   createMonthYearEl(new Date());
   createAndDisplayDays();
