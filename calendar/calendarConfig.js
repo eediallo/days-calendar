69c54f72f54ec71f39c3b5afd67a10689113cfa9
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 14:23:14 +0000

    Adjust startingDay calculation to align with week structure

--- calendar/calendarConfig.js
+++ calendar/calendarConfig.js
@@ -3,7 +3,8 @@ const year = date.getFullYear();
 const month = date.getMonth();
 const firstDayOfMonth = new Date(year, month, 1);
 const daysInMonth = new Date(year, month + 1, 0).getDate();
-const startingDay = firstDayOfMonth.getDay();
+const startingDay =
+  firstDayOfMonth.getDay() === 0 ? 6 : firstDayOfMonth.getDay() - 1;
 
 export const calendarConfig = {
   date,
