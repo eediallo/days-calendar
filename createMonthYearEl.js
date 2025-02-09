be3af9eafe039b25231d15079501c446ba014ea3
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 12:49:23 +0000

    move createMonthYearEl and data modules to streamline codebase

--- createMonthYearEl.js
+++
@@ -1,9 +0,0 @@
-import { monthYearEl } from "./queries.js";
-import { monthNames } from "./data.js";
-
-const month = (date) => date.getMonth();
-const year = (date) => date.getFullYear();
-
-export function createMonthYearEl(date) {
-  monthYearEl.textContent = `${monthNames[month(date)]} ${year(date)}`;
-}
