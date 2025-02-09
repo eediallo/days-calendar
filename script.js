c76499f0f1dc4085394a33b84a6089721e53ac43
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 12:44:34 +0000

    Call createMonthYearEl function in main to display the current month and year

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { createAndDisplayDays } from "./createAndDisplayDays.js";
 import { createAndDisplayWeekDays } from "./createAndDisplayWeekDays.js";
+import { createMonthYearEl } from "./createMonthYearEl.js";
 function main() {
+  createMonthYearEl(new Date());
   createAndDisplayDays();
   createAndDisplayWeekDays();
 }
