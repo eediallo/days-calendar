d884174f09d4e1e32378f537068c92230e48890d
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 21:12:02 +0000

    Add renderMonthList call in main function to initialize month selection

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import { calendarConfig } from "./calendar/calendarConfig.js";
 import { displayCalendar } from "./calendar/displayCalendar.js";
 import { eventHandlers } from "./events/events.js";
+import { renderMonthList } from "./jump/renderMonthList.js";
 
 function main() {
   displayCalendar(calendarConfig.currentDate);
   eventHandlers();
+  renderMonthList();
 }
 window.onload = main;
