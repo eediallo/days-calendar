e76bf5d565b89586d21c90912ddc945f1c469c31
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 22:21:30 +0000

    Add goBtn event listener to trigger jumpHandler function

--- events/events.js
+++ events/events.js
@@ -1,8 +1,10 @@
-import { nexMonthEl, prevMonthEl } from "../queries.js";
+import { goBtn, nexMonthEl, prevMonthEl } from "../queries.js";
+import { jumbHandler } from "./jumpHandler.js";
 import { nextMonthHandler } from "./nextMonthHandler.js";
 import { prevMonthHandler } from "./prevMonthHandler.js";
 
 export function eventHandlers() {
   prevMonthEl.addEventListener("click", prevMonthHandler);
   nexMonthEl.addEventListener("click", nextMonthHandler);
+  goBtn.addEventListener("click", jumbHandler);
 }
