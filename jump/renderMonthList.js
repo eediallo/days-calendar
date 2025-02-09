2bdb5c1c885c2db2395bf374202c62c714a88b2f
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-09 21:20:26 +0000

    Refactor renderMonthList to use createOption for generating month options

--- jump/renderMonthList.js
+++ jump/renderMonthList.js
@@ -1,8 +1,8 @@
 import { monthNames } from "../data/data.js";
 import { monthSelect } from "../queries.js";
-import { createMonthOption } from "./createMonthOption.js";
+import { createOption } from "./createOption.js";
 
 export function renderMonthList() {
-  const optionList = monthNames.map(createMonthOption);
+  const optionList = monthNames.map((month) => createOption(month));
   monthSelect.append(...optionList);
 }
