67c5985ada80b71fd5affb22c75b7b708390e661
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-10 11:06:23 +0000

    Add unit test for getMonth function in calendarConfig

---
+++ tests/getMonth.test.js
@@ -0,0 +1,9 @@
+import { calendarConfig } from "../calendar/calendarConfig.js";
+
+describe("getMonth", () => {
+  test("should return current month", () => {
+    const curruntDate = new Date();
+    const currentInput = calendarConfig.getMonth(curruntDate);
+    expect(currentInput).toBe(1);
+  });
+});
