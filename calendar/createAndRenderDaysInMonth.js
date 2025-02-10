3a71fb67c8cd40514ca8a06268b33edc5bce1446
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-10 13:23:42 +0000

    Add commemorative day highlighting in calendar rendering

--- calendar/createAndRenderDaysInMonth.js
+++ calendar/createAndRenderDaysInMonth.js
@@ -1,3 +1,4 @@
+import { getCommemorativeDay } from "../comDays/getComDay.js";
 import { calendarContainerEl } from "../queries.js";
 import { calendarConfig } from "./calendarConfig.js";
 
@@ -5,6 +6,11 @@ function createDayMonth(day) {
   const dayElement = document.createElement("div");
   dayElement.textContent = day;
   dayElement.classList.add("calendar-day");
+  const commemorativeDay = getCommemorativeDay(day);
+  if (commemorativeDay) {
+    dayElement.classList.add("special");
+    dayElement.setAttribute("data-tooltip", commemorativeDay.name);
+  }
   return dayElement;
 }
 
