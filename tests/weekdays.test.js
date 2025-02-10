4b7e7d0784b227a8e5ff58a17678efbcd1f5d8a7
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-10 11:24:30 +0000

    Add unit tests for weekdays array in weekdays.test.js

---
+++ tests/weekdays.test.js
@@ -0,0 +1,19 @@
+import { type } from "node:os";
+import { weekdays } from "../data/data.js";
+
+describe("weekdays", () => {
+  test("should be an array", () => {
+    const isArray = Array.isArray(weekdays);
+    expect(isArray).toBe(true);
+  });
+
+  test("should be of length 7", () => {
+    expect(weekdays.length === 7).toBe(true);
+  });
+
+  test("Elements should be of type string", () => {
+    weekdays.forEach((day) => {
+      expect(typeof day === "string").toBe(true);
+    });
+  });
+});
