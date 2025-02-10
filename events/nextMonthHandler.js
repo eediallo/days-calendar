3bac47965cf601ea2ecf904b31d094ef42e4ac21
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-02-10 22:34:29 +0000

    Updated nextMonthHandler to display commemorative days

--- events/nextMonthHandler.js
+++ events/nextMonthHandler.js
@@ -1,11 +1,16 @@
 import { calendarConfig } from "../calendar/calendarConfig.js";
 import { calendarContainerEl } from "../queries.js";
 import { displayCalendar } from "../calendar/displayCalendar.js";
+import { loadCommemorativeDays } from "../CommemorativeDays/loadCommemorativeDays.js"; 
 
-export function nextMonthHandler() {
+
+export async function nextMonthHandler() {
   calendarContainerEl.innerHTML = "";
   calendarConfig.currentDate.setMonth(
     calendarConfig.currentDate.getMonth() + 1
   );
+
+  await loadCommemorativeDays(calendarConfig.currentDate.getFullYear());
+  
   displayCalendar(calendarConfig.currentDate);
 }
