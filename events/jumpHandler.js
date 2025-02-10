e41974e67d16d75f74a6672c27c7726d5c051e4a
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-02-10 19:25:39 +0000

    Updated loadCommemorativeDays

--- events/jumpHandler.js
+++ events/jumpHandler.js
@@ -1,11 +1,16 @@
 import { calendarContainerEl, monthSelect, yearSelect } from "../queries.js";
 import { displayCalendar } from "../calendar/displayCalendar.js";
 import { calendarConfig } from "../calendar/calendarConfig.js";
-
-export function jumpHandler() {
+import { loadCommemorativeDays } from "../CommemorativeDays/loadCommemorativeDays.js"; 
+export async function jumpHandler() {
   calendarContainerEl.innerHTML = "";
-  const selectedMonth = parseInt(monthSelect.value);
-  const selectedYear = parseInt(yearSelect.value);
-  calendarConfig.currentDate = new Date(selectedYear, selectedMonth, 1); // Update the current date
+
+  const selectedMonth = parseInt(monthSelect.value, 10); 
+  const selectedYear = parseInt(yearSelect.value, 10); 
+
+  calendarConfig.currentDate = new Date(selectedYear, selectedMonth, 1);
+
+  await loadCommemorativeDays(selectedYear);
+
   displayCalendar(calendarConfig.currentDate);
 }
