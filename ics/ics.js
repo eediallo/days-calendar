a260df47bcc8f9fb736f2698059a81fe7f39de9a
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-10 17:05:22 +0000

    Add function to generate iCal entries for commemorative days

--- ics/ics.js
+++ ics/ics.js
@@ -39,3 +39,22 @@ function getCommemorativeDayDate(year, commemorativeDay) {
 
   return calendarConfig.currentDate;
 }
+
+// generateICalEntry generates an iCal entry for a special day
+function generateICalEntry(commemorativeDay, date) {
+  const startDate = date.toISOString().replace(/-|:|\.\d\d\d/g, "");
+  console.log(startDate, "STARTDATE==========>");
+  const endDate = new Date(date.getTime() + 24 * 60 * 60 * 1000)
+    .toISOString()
+    .replace(/-|:|\.\d\d\d/g, ""); // Add 1 day
+
+  return `
+      BEGIN:VEVENT
+      UID:${Math.random().toString(36).substring(2)}@commemorativeDays.com
+      DTSTAMP:${startDate}
+      DTSTART;VALUE=DATE:${startDate.slice(0, 8)}
+      DTEND;VALUE=DATE:${endDate.slice(0, 8)}
+      SUMMARY:${commemorativeDay.name}
+      DESCRIPTION:${commemorativeDay.descriptionURL}
+      END:VEVENT`;
+}
