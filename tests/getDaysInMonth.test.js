fe1d51b447985d99c9cbc1996c1b527440522426
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-10 11:19:02 +0000

    Fix test suite description for getDaysInMonth function

--- tests/getDaysInMonth.test.js
+++ tests/getDaysInMonth.test.js
@@ -1,7 +1,7 @@
 import { calendarConfig } from "../calendar/calendarConfig.js";
 
-describe("getMonth", () => {
-  test("should return current month", () => {
+describe("getDaysInMonth", () => {
+  test("should return number of days of current Month", () => {
     const curruntDate = new Date();
     const currentInput = calendarConfig.getDaysInMonth(curruntDate);
     expect(currentInput).toBe(28);
