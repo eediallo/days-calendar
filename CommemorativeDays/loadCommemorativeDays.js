89bc4af1ae21ec65c787c8819b578237bc6fee21
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-02-10 19:23:48 +0000

    Updated

--- CommemorativeDays/loadCommemorativeDays.js
+++ CommemorativeDays/loadCommemorativeDays.js
@@ -1,22 +1,32 @@
-import { getDayOfWeekIndex, getFirstDayOfMonth, calculateOccurrenceDate } from './CommemorativeDaysFunctions.js';
+import { calculateOccurrenceDate } from './CommemorativeDaysFunctions.js';
 
-export async function loadCommemorativeDays() {
+export let commemorativeDays = [];
+
+export async function loadCommemorativeDays(year) {
   try {
     const response = await fetch('./data/days.json');
     const data = await response.json();
 
+    commemorativeDays.length = 0;
+
     data.forEach((day) => {
-      const year = day.year || 2024;
-      const month = new Date(Date.parse(day.monthName + " 1, " + year)).getMonth(); 
+      const month = new Date(Date.parse(`${day.monthName} 1, ${year}`)).getMonth(); 
 
       const commemorativeDate = calculateOccurrenceDate(month, year, day.dayName, day.occurence);
 
-      // Will be removed later
-      console.log(`${day.name} falls on ${commemorativeDate.toDateString()}`); 
+      if (!isNaN(commemorativeDate)) {
+        commemorativeDays.push({
+          name: day.name,
+          date: commemorativeDate,
+          descriptionURL: day.descriptionURL,
+        });
+        
+        console.log(`${day.name} in ${year} falls on ${commemorativeDate.toDateString()}`);
+      } else {
+        console.error(`Invalid commemorative date for ${day.name}`);
+      }
     });
   } catch (error) {
     console.error('Error loading JSON data:', error);
   }
 }
-
-
