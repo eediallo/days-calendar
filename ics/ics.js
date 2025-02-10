1988c60e5edf8a33633bebf1bab75422dbfb97f3
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-10 17:12:05 +0000

    Refactor iCal entry generation for improved readability and remove unnecessary console log

--- ics/ics.js
+++ ics/ics.js
@@ -12,7 +12,7 @@ function getCommemorativeDayDate(year, commemorativeDay) {
 
   calendarConfig.currentDate = new Date(year, month, 1);
 
-  // Find the first occurrence of the target day
+
   while (calendarConfig.currentDate.getDay() !== weekdays.indexOf(dayName)) {
     calendarConfig.currentDate.setDate(
       calendarConfig.currentDate.getDate() + 1
@@ -43,13 +43,11 @@ function getCommemorativeDayDate(year, commemorativeDay) {
 // generateICalEntry generates an iCal entry for a special day
 function generateICalEntry(commemorativeDay, date) {
   const startDate = date.toISOString().replace(/-|:|\.\d\d\d/g, "");
-  console.log(startDate, "STARTDATE==========>");
   const endDate = new Date(date.getTime() + 24 * 60 * 60 * 1000)
     .toISOString()
     .replace(/-|:|\.\d\d\d/g, ""); // Add 1 day
 
-  return `
-      BEGIN:VEVENT
+  return `BEGIN:VEVENT
       UID:${Math.random().toString(36).substring(2)}@commemorativeDays.com
       DTSTAMP:${startDate}
       DTSTART;VALUE=DATE:${startDate.slice(0, 8)}
@@ -61,10 +59,10 @@ function generateICalEntry(commemorativeDay, date) {
 
 function generateICalFile() {
   let icalContent = `BEGIN:VCALENDAR
-    VERSION:2.0
-    PRODID:-//Special Days Calendar//EN
-    CALSCALE:GREGORIAN
-    METHOD:PUBLISH
+     VERSION:2.0
+     PRODID:-//Special Days Calendar//EN
+     CALSCALE:GREGORIAN
+     METHOD:PUBLISH
     `;
 
   for (let year = 2020; year <= 2030; year++) {
