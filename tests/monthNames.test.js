33d785e46f59bfc374c5faf4f00f17a9e09dfc4c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-10 11:33:35 +0000

    Add additional unit tests for monthNames array

--- tests/monthNames.test.js
+++ tests/monthNames.test.js
@@ -5,4 +5,16 @@ describe("MonthNames", () => {
     const isArray = Array.isArray(monthNames);
     expect(isArray).toBe(true);
   });
+
+  test("should have 12 elements", () => {
+    expect(monthNames.length === 12).toBe(true);
+  });
+
+  test("Each month should have an id", () => {
+    monthNames.forEach((month) => expect(month).toHaveProperty("id"));
+  });
+
+  test("Each month should have a name", () => {
+    monthNames.forEach((month) => expect(month).toHaveProperty("name"));
+  });
 });
