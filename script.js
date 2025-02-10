dd54c02da523b52cf78d72339c8498ba7e5027c0
Ameneh-KM <ameneh.keshavarz@gmail.com>
2025-02-10 19:24:55 +0000

    Updated

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ import { calendarConfig } from "./calendar/calendarConfig.js";
 import { displayCalendar } from "./calendar/displayCalendar.js";
 import { eventHandlers } from "./events/events.js";
 import { renderDropDowns } from "./jump/renderDropDown.js";
-import {loadCommemorativeDays} from "./CommemorativeDays/loadCommemorativeDays.js";
+import { loadCommemorativeDays } from "./CommemorativeDays/loadCommemorativeDays.js";
 
-function main() {
-  displayCalendar(calendarConfig.currentDate);
-  eventHandlers();
-  renderDropDowns();
-  loadCommemorativeDays();
+async function main() {
+  const currentYear = new Date().getFullYear();
+  await loadCommemorativeDays(currentYear);  
+  displayCalendar(calendarConfig.currentDate);  
+  eventHandlers();  
+  renderDropDowns();  
 }
 window.onload = main;
-
-
