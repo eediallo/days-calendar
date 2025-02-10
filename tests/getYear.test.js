051ff7d59378104e1d8b77b1c010261592b466c3
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-10 11:07:34 +0000

    Add unit test for getYear function in calendarConfig

---
+++ tests/getYear.test.js
@@ -0,0 +1,9 @@
+import { calendarConfig } from "../calendar/calendarConfig.js";
+
+describe("getYear", () => {
+  test("should return current year", () => {
+    const curruntDate = new Date();
+    const currentInput = calendarConfig.getYear(curruntDate);
+    expect(currentInput).toBe(2025);
+  });
+});
