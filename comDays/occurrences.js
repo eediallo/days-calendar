6e0a9640e8e58e0be372ff4633ceb72e963af459
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-11 13:41:36 +0000

    Refactor occurrence functions to simplify return values and remove unnecessary parameters

--- comDays/occurrences.js
+++ comDays/occurrences.js
@@ -1,31 +1,19 @@
-export function firstOccurrence(occurence, dayOfMonth, commemorativeDayDay) {
-  return occurence === "first" && dayOfMonth <= 7 ? commemorativeDayDay : null;
+export function firstOccurrence(occurence, dayOfMonth) {
+  return occurence === "first" && dayOfMonth <= 7;
 }
 
-export function secondOccurrence(occurence, dayOfMonth, commemorativeDayDay) {
-  return occurence === "second" && dayOfMonth > 7 && dayOfMonth <= 14
-    ? commemorativeDayDay
-    : null;
+export function secondOccurrence(occurence, dayOfMonth) {
+  return occurence === "second" && dayOfMonth > 7 && dayOfMonth <= 14;
 }
 
-export function thirdOccurrence(occurence, dayOfMonth, commemorativeDayDay) {
-  return occurence === "third" && dayOfMonth > 14 && dayOfMonth <= 21
-    ? commemorativeDayDay
-    : null;
+export function thirdOccurrence(occurence, dayOfMonth) {
+  return occurence === "third" && dayOfMonth > 14 && dayOfMonth <= 21;
 }
 
-export function lastOccurrence(
-  occurence,
-  dayOfMonth,
-  commemorativeDayDay,
-  year,
-  month
-) {
+export function lastOccurrence(occurence, dayOfMonth, year, month) {
   if (occurence === "last") {
     const lastDayOfMonth = new Date(year, month + 1, 0).getDate();
-    if (dayOfMonth > lastDayOfMonth - 7) {
-      return commemorativeDayDay;
-    }
+    return dayOfMonth > lastDayOfMonth - 7;
   }
-  return null;
+  return false;
 }
