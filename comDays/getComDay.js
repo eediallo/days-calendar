9aad11f98a4792858ff4fab9f11ca2c6da889ea8
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-11 13:41:56 +0000

    Refactor getCommemorativeDay function to improve readability and streamline occurrence checks

--- comDays/getComDay.js
+++ comDays/getComDay.js
@@ -1,34 +1,35 @@
 import { calendarConfig } from "../calendar/calendarConfig.js";
 import { commemorativeDays } from "../data/comDays.js";
 import { monthNames } from "../data/data.js";
-import { firstOccurrence } from "./firstOccurrence.js";
-import { secondOccurrence } from "./secondOccurence.js";
-import { thirdOccurrence } from "./thirdOccurrence.js";
-import { lastOccurrence } from "./lastOccurrence.js";
+import {
+  firstOccurrence,
+  secondOccurrence,
+  thirdOccurrence,
+  lastOccurrence,
+} from "./occurrences.js";
 
 export function getCommemorativeDay(day) {
   const year = calendarConfig.getYear(calendarConfig.currentDate);
   const month = calendarConfig.getMonth(calendarConfig.currentDate);
 
   const date = new Date(year, month, day);
-  const dayName = date.toLocaleDateString("en-US", { weekday: "long" });
+  const dayName = date.toLocaleDateString("en-GB", { weekday: "long" });
 
-  for (const commemorativeDayDay of commemorativeDays) {
+  for (const commemorativeDay of commemorativeDays) {
     if (
-      commemorativeDayDay.monthName === monthNames[month].name &&
-      commemorativeDayDay.dayName === dayName
+      commemorativeDay.monthName === monthNames[month].name &&
+      commemorativeDay.dayName === dayName
     ) {
-      const occurence = commemorativeDayDay.occurence;
+      const occurence = commemorativeDay.occurence;
       const dayOfMonth = date.getDate();
 
-      const result =
-        firstOccurrence(occurence, dayOfMonth, commemorativeDayDay) ||
-        secondOccurrence(occurence, dayOfMonth, commemorativeDayDay) ||
-        thirdOccurrence(occurence, dayOfMonth, commemorativeDayDay) ||
-        lastOccurrence(occurence, dayOfMonth, commemorativeDayDay, year, month);
-
-      if (result) {
-        return result;
+      if (
+        firstOccurrence(occurence, dayOfMonth) ||
+        secondOccurrence(occurence, dayOfMonth) ||
+        thirdOccurrence(occurence, dayOfMonth) ||
+        lastOccurrence(occurence, dayOfMonth, year, month)
+      ) {
+        return commemorativeDay;
       }
     }
   }
