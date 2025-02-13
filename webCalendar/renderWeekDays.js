101aa03eafcf7f74d96a0eb1819fd078c496198c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-13 13:58:16 +0000

    Add missing newline for improved code readability in renderWeekDays.js

--- webCalendar/renderWeekDays.js
+++ webCalendar/renderWeekDays.js
@@ -1,6 +1,7 @@
 import { createWeekDayEl } from "./createWeekDayEl.js";
 import { weekdays } from "../data/data.js";
 import { calendarContainerEl } from "../config/queries.js";
+
 function renderWeekdays() {
   const daysList = weekdays.map(createWeekDayEl);
   calendarContainerEl.append(...daysList);
