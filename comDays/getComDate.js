9e6116b30b2f2ff1d5439ca5faf1274921ac2863
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-21 21:35:44 +0000

    Add JSDoc comments for getCommemorativeDayDate function to improve documentation

--- comDays/getComDate.js
+++ comDays/getComDate.js
@@ -6,6 +6,16 @@ import {
   isLastOccurrence,
 } from "./occurrences.js";
 
+/**
+ * Get the date of a commemorative day for a given year.
+ *
+ * @param {number} year - The year for which to find the commemorative day.
+ * @param {Object} commemorativeDay - An object representing the commemorative day.
+ * @param {string} commemorativeDay.monthName - The name of the month (e.g., "January").
+ * @param {string} commemorativeDay.dayName - The name of the day (e.g., "Monday").
+ * @param {number} commemorativeDay.occurence - The occurrence of the day in the month (e.g., 1 for first, 2 for second, etc.).
+ * @returns {Date} The date of the commemorative day.
+ */
 export function getCommemorativeDayDate(year, commemorativeDay) {
   const months = monthNames.map((month) => month.name);
   const month = months.indexOf(commemorativeDay.monthName);
