2940ca3d4e3f975278d0810a16331d1d1572fa51
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-21 21:43:20 +0000

    Add JSDoc comments to attachEventHandlers function for improved documentation

--- eventHandlers/attachEventHandlers.js
+++ eventHandlers/attachEventHandlers.js
@@ -5,6 +5,14 @@ import {
   prevMonthHandler,
 } from "./eventHandlers.js";
 
+/**
+ * Attaches event handlers to the previous month, next month, and go buttons.
+ * 
+ * This function adds click event listeners to the following buttons:
+ * - `prevMonthBtn`: Triggers the `prevMonthHandler` function when clicked.
+ * - `nexMonthBtn`: Triggers the `nextMonthHandler` function when clicked.
+ * - `goBtn`: Triggers the `jumpToNextMonthAndYearHandler` function when clicked.
+ */
 export function attachEventHandlers() {
   prevMonthBtn.addEventListener("click", prevMonthHandler);
   nexMonthBtn.addEventListener("click", nextMonthHandler);
