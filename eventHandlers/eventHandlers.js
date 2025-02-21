7f3abd63d18116b56cb1a6dcc2cfabf8c2122d99
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-21 21:42:46 +0000

    Add JSDoc comments for month navigation handlers to improve documentation

--- eventHandlers/eventHandlers.js
+++ eventHandlers/eventHandlers.js
@@ -6,6 +6,11 @@ import {
 import { createAndDisplayCalendar } from "../webCalendar/createAnddisplayCalendar.js";
 import { datesConfig } from "../config/datesConfig.js";
 
+/**
+ * Handles the event to jump to the next month and year in the calendar.
+ * Clears the current calendar display, updates the current date to the selected month and year,
+ * and creates and displays the new calendar for the updated date.
+ */
 export function jumpToNextMonthAndYearHandler() {
   calendarContainerEl.innerHTML = "";
   const selectedMonth = parseInt(monthSelect.value);
@@ -14,12 +19,22 @@ export function jumpToNextMonthAndYearHandler() {
   createAndDisplayCalendar(datesConfig.currentDate);
 }
 
+/**
+ * Handles the event to navigate to the next month in the calendar.
+ * Clears the current calendar display, updates the current date to the next month,
+ * and creates and displays the updated calendar.
+ */
 export function nextMonthHandler() {
   calendarContainerEl.innerHTML = "";
   datesConfig.currentDate.setMonth(datesConfig.currentDate.getMonth() + 1);
   createAndDisplayCalendar(datesConfig.currentDate);
 }
 
+/**
+ * Handles the event to navigate to the previous month in the calendar.
+ * Clears the current calendar display, updates the current date to the previous month,
+ * and then creates and displays the updated calendar.
+ */
 export function prevMonthHandler() {
   calendarContainerEl.innerHTML = "";
   datesConfig.currentDate.setMonth(datesConfig.currentDate.getMonth() - 1);
