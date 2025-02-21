5fc78b2a090194ef10349eaf20700c2dabcd59ad
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-21 21:50:07 +0000

    Add JSDoc comments to createMonthYearEl function for improved documentation

--- webCalendar/createMonthYearEl.js
+++ webCalendar/createMonthYearEl.js
@@ -2,6 +2,11 @@ import { monthYearEl } from "../config/queries.js";
 import { monthNames } from "../data.js";
 import { datesConfig } from "../config/datesConfig.js";
 
+/**
+ * Creates and sets the text content of an element to display the month and year based on the provided date.
+ *
+ * @param {Date} date - The date object from which to extract the month and year.
+ */
 export function createMonthYearEl(date) {
   const month = datesConfig.getMonth(date);
   const year = datesConfig.getYear(date);
