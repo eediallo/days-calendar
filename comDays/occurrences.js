53eb9ab41eb3a65ae6e4bd1a0984528a3d7b4bf2
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-21 21:43:11 +0000

    Add JSDoc comments for isLastOccurrence function to enhance documentation

--- comDays/occurrences.js
+++ comDays/occurrences.js
@@ -10,6 +10,15 @@ export function isThirdOccurrence(occurence, dayOfMonth) {
   return occurence === "third" && dayOfMonth > 14 && dayOfMonth <= 21;
 }
 
+/**
+ * Checks if the given occurrence is the last occurrence of the specified day in the month.
+ *
+ * @param {string} occurence - The occurrence type, expected to be "last".
+ * @param {number} dayOfMonth - The day of the month to check.
+ * @param {number} year - The year of the date.
+ * @param {number} month - The month of the date (0-based, where 0 is January and 11 is December).
+ * @returns {boolean} - Returns true if the dayOfMonth is within the last 7 days of the month, otherwise false.
+ */
 export function isLastOccurrence(occurence, dayOfMonth, year, month) {
   if (occurence === "last") {
     const lastDayOfMonth = new Date(year, month + 1, 0).getDate();
